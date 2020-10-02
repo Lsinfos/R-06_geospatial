7b1e974df946c2bc804b4f7c47e0b106f42027f5
Trang Ng <59824573+ltrangng@users.noreply.github.com>
2020-10-03 00:11:25 +0200

    plyr

--- 1_code/06_Packages.R
+++ 1_code/06_Packages.R
@@ -35,3 +35,46 @@ head(dat2) # This form is ideal to answer question like which month is the warme
 dat2 <- dcast(dat_melt, year ~ variable, mean, na.rm = TRUE)
 head(dat2)
 # The disadvantage of aggregation with dcast() is that we must apply the same function across all variables.
+
+# Aggregating with plyr ####
+
+# The plyr package is intended to comprise an alternative, in many cases an easier one, to apply and other base R functions.
+# ddply function operates on a data frame, splits the table to subsets according to the unique levels in one or more columns. The data from each subset is then used to calculate a single value, for each of the new columns in the new data frame.
+# E.g, calculate the average area size of a flower's petals and sepals per species
+ddply(iris, .(Species), summarize,
+      sepal_area = mean(Sepal.Length * Sepal.Width),
+      petall_area = mean(Petal.Length * Petal.Width))
+
+# ddply() contains several arguments:
+# input data frame (e.g: iris)
+# the name(s) of the column(s), which defines subsets, in () and preceded by "." (more names will be separated by commas).
+# mode of operation: summarize (new columns form a new, aggregrated, table), and transform (new columns are appended back to the input table).
+# 4th argument and onward: user-specified extensions for calculation of new columns based on values in the original column.
+
+# Calculate the iris data frame again with transform mode
+ddply(iris, .(Species), transform,
+      sepal_area = mean(Sepal.Length * Sepal.Width),
+      petall_area = mean(Petal.Length * Petal.Width)) # original table is preserved, with 2 new columns joined in.
+
+# Use ddply() to aggregate the climatic data table from a monthly to an annual timescale. Firs filter out those variable/year combinations where not all the 12 months are present.
+dat3 <- ddply(dat_melt, .(station, year, variable), transform,
+      months_available = length(value[!is.na(value)])) # remove NAs.
+head(dat3)
+# Using the new colum "month_available" to remove the combination with less than 12 months of data in order to reduce the bias in the annual average
+dat3 <- dat3[dat3$months_available == 12, ]
+# Next create a table to only hold the location data (latitude, longitude, elevation) for each metorological station
+spain_stations <- ddply(dat3, .(station), summarize,
+                        latitude = latitude[1],
+                        longitude = longitude[1],
+                        elevation = elevation[1])
+head(spain_stations) # it's frequently useful to have such table, e.g, to plot the stations' spatial locations.
+# Save this data frame for later use
+write.csv(spain_stations, "spain_stations.csv", row.names = FALSE)
+# Now aggregate the climatic data to find the sum of each month
+spain_annual <- ddply(dat3, .(station, variable, year), summarize,
+                      value = ifelse(variable[1] == "tpcp", 
+                      sum(value, na.rm = TRUE),
+                      mean(value, na.rm = TRUE)))
+head(spain_annual)
+# Save this data frame for later use, too
+write.csv(spain_annual, "spain_annual.csv", row.names = FALSE)
