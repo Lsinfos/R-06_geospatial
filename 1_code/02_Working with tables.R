a1ac40292f6fb4af7de5daf2911ba2acde559b93
Trang Ng <59824573+ltrangng@users.noreply.github.com>
2021-03-16 23:17:21 +0100

    working with tables

--- 1_code/02_Working with tables.R
+++ 1_code/02_Working with tables.R
@@ -1,73 +1,66 @@
-# 4. TABLES
+# 2. WORKING WITH TABLES
 
-# Working with tables is central to programming in R, both with regards to spatial analysis (e.g, working with attribute tables of geometries).
-
-# 4.1 Data.frame class ####
-
-# The data.frame class is the basic class to represent tabular data in R. A data.frame object is essentially a collection of vectors, all with the same length. The vectors don't have to be the same type, therefore, data.frame object is particularly suitable to represent data with different variables in columns and different cases in rows.
-# Creating a data.frame object by combining available vectors
+# 2.1 Using data frames to represent tabular data ####
+# The data.frame class is the basic class to represent tabular data in R. A data frame is essentially a collection of vectors, all with the same length. The vectors don't have to be the same type, therefore, data frame is particularly suitable to represent data with different variables in columns and different cases in rows.
+# Creating a data frame by combining available vectors:
 num <- 1:4
 lower <- c("a", "b", "c", "d")
 upper <- c("A", "B", "C", "D")
 df <- data.frame(num, lower, upper)
-df # the names of the columns are the names of the original vectors.
-# The data.frame object could be created in a single step
+df # the columns' names are the names of the original vectors.
+# The data frame could be created in a single step.
 df <- data.frame(num = 1:4, 
                  lower = c("a", "b", "c", "d"),
                  upper = c("A", "B", "C", "D"))
-# Adding rows to an existing data.frame object using rbind() functions
+# Adding rows to an existing data frames using rbind():
 row5 <- c(5, "e", "E")
 rbind(df, row5)
-# Alternatively, add a fourth column using cbind() function
+# Alternatively, add a fourth column using cbind():
 word <- c("One", "Two", "Three", "Four") 
 cbind(df, word)
 
-# 4.2 Import CSV files ####
-
-# Another common method to create data.frame object is importing tabular data from a CSV file using read.csv() function
+# 2.2 Creating a table from a csv file ####
+# Another common method to create data.frame object is importing tabular data from a CSV file using read.csv().
 dat <- read.csv("Spain.csv") # contains monthly records of precipitation and temperature in Spain for a periods of 30 years. 
-# Print only a few first rows of the data frame, as this table is very large
+# Print only a few first rows of the data frame to examine, as this table is very large:
 head(dat)
-# Similarly, tail() function prints last few rows
+# Similarly, tail() prints last few rows.
 tail(dat)
 # The columns' contents:
-# STATION: the meteorological station identificaion code
+# STATION: the meteorological station identification code
 # STATION_NAME: the meteorological station's name
 # ELEVATION: the elevation above sea level of the station (in meters)
-# LATTITUDE: the lattitude of the station (decimal degrees)
+# LATTITUDE: the latitude of the station (decimal degrees)
 # LONGITUDE: the longitude of the station (decimal degrees)
 # DATE: the date of the measurement
-# TPCP: total monthly precipation (0.1mm unit)
+# TPCP: total monthly precipitation (0.1mm unit)
 # MMXT: the mean monthly maximum temperature (0.1 degree Celcius units)
 # MMNT: the mean monthly minimum temperature (0.1 degree Celcius units)
 
-# 4.3 Examine the structure of a data.frame object ####
-
-# Get the number of rows and columns using nrow() and ncol() functions, respectively. 
+# 2.3 Examine the structure of a data frame####
+# Get the number of rows and columns using nrow() and ncol() functions, respectively: 
 nrow(dat)
 ncol(dat)
-# Or get the lengths of both row and column dimensions using dim() function
+# Or get the lengths of both row and column using dim():
 dim(dat)
-# Get the names of the columns as a character vector 
+# Get the names of the columns as a character vector: 
 colnames(dat)
 # Alternatively, rownames() could be used to get the name of the rows, but colnames() is more common.
-# Assignment into column names can be made an existing name with a new one
+# Assignment into column names can be made an existing name with a new one.
 colnames(dat)[3] <- "Elev"
-# Similarly, convert all column names from uppercase to lowercase using tolower() function
+# Similarly, convert all column names from uppercase to lowercase using tolower():
 colnames(dat) <- tolower(colnames(dat))
-# Check again
+# Check again:
 colnames(dat)
-
 # It is frequently useful to examine the structure of a given object using the str(). This function prints the structure of its argument showing the data types of its components and the relations between them. 
 str(dat)
 
-# 4.4 Subsetting data.frame objects ####
-
-# Subsetting a data frame by accessing separate columns with $ operator
+# 2.4 Subsetting data frames ####
+# Subsetting a data frame by accessing separate columns with $ operator:
 df$num
 df$lower
 df$upper
-# Replace the missing values of measurements which are marked as -9999, a commonl encountered convention, with NA
+# Replace the missing values of measurements which are marked as -9999, a common encountered convention, with NA:
 dat$tpcp[dat$tpcp == -9999] <- NA
 dat$mmxt[dat$mmxt == -9999] <- NA
 dat$mmnt[dat$mmnt == -9999] <- NA # $ operator refers to columns of the data.frame object (dat) rather than independent vectors.
@@ -76,13 +69,13 @@ dat$tpcp <- dat$tpcp / 10
 dat$mmxt <- dat$mmxt / 10
 dat$mmnt <- dat$mmnt / 10
 
-# Using [] operator for indexing a data.frame object requires 2 dimensions
+# Using [] operator for indexing a data.frame object requires two dimensions:
 df[2,3] # first index refers to rows, second index refers to columns.
-# Leaving and empty space instead of the row's or column's index indicates selecting all elements of the respective dimension
+# Leaving an empty space instead of the row's or column's index indicates selecting all elements of the respective dimension.
 df[2,] # returns a data frame since 3 columns are involved.
 df[,3] # returns a vector.
 is.data.frame(df[,3])
-# The parameter "drop" can be used to suppress the data frame simplification to remain a data frame no matter what
+# The parameter "drop" can be used to suppress the data frame simplification to remain a data frame no matter what.
 df[ ,3, drop = FALSE]
 
 # Using a character vector that indicates the names of the rows/columns
@@ -94,8 +87,7 @@ dat[complete.cases(dat),]
 # or to locate the cases with NA value
 dat[!complete.cases(dat),]
 
-# 4.5 Calculating new data fields ####
-
+# 2.5 Calculating new data fields ####
 # New values can be assigned to a column with $ operator. If the assigned column does not exist in the table, a new column will be created to accomodate the data
 df$word[df$num == 2] <- "Two"
 df
